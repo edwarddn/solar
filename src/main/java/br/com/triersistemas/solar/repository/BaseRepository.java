62a3d3e6a671f838dfae2e5aea88defc37152060
Edward <edward.neto@triersistemas.com.br>
2022-06-15 14:38:17 -0300

    ajuste

--- src/main/java/br/com/triersistemas/solar/repository/BaseRepository.java
+++ src/main/java/br/com/triersistemas/solar/repository/BaseRepository.java
@@ -3,14 +3,10 @@ package br.com.triersistemas.solar.repository;
 import java.util.List;
 import java.util.Optional;
 
-public interface BaseRepository<ID, DOMAIN> {
-    List<DOMAIN> consultar();
-
-    Optional<DOMAIN> consultar(ID id);
-
-    List<DOMAIN> consultar(List<ID> ids);
-
-    void cadastrar(DOMAIN domain);
-
-    void remover(DOMAIN domain);
+public interface BaseRepository<ID, T> {
+    List<T> consultar();
+    Optional<T> consultar(ID id);
+    List<T> consultar(List<ID> ids);
+    void cadastrar(T domain);
+    void remover(T domain);
 }
