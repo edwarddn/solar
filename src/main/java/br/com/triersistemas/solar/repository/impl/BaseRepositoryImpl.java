f124d4134ac6c5055fa56fe1e491c68c5374c8bf
Edward <edward.neto@triersistemas.com.br>
2022-06-15 13:27:41 -0300

    melhorias

--- src/main/java/br/com/triersistemas/solar/repository/impl/BaseRepositoryImpl.java
+++ src/main/java/br/com/triersistemas/solar/repository/impl/BaseRepositoryImpl.java
@@ -1,75 +1,69 @@
 package br.com.triersistemas.solar.repository.impl;
 
 import br.com.triersistemas.solar.exceptions.NaoExisteException;
+import br.com.triersistemas.solar.exceptions.NotImplementedException;
 import br.com.triersistemas.solar.repository.BaseRepository;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
 import java.util.*;
 
-public class BaseRepositoryImpl<ID, DOMAIN> implements BaseRepository<ID, DOMAIN> {
+public class BaseRepositoryImpl<ID, T> implements BaseRepository<ID, T> {
 
-    private static final Map<String, List> MAP = new HashMap();
+    private static final Map<String, List<?>> MAP_DB = new HashMap<>();
 
-    private final Class<ID> typeId;
-    private final Class<DOMAIN> typeDomain;
-
-    private final List<DOMAIN> list;
-
-    private List<DOMAIN> getList(final String key) {
-        var list = MAP.get(key);
-        if (Objects.isNull(list)) {
-            list = new ArrayList<>();
-            MAP.put(key, list);
-        }
-        return list;
-    }
+    private final List<T> list;
 
     public BaseRepositoryImpl() {
-        this.typeId = (Class<ID>) ((ParameterizedType) getClass()
-                .getGenericSuperclass())
-                .getActualTypeArguments()[0];
-        this.typeDomain = (Class<DOMAIN>) ((ParameterizedType) getClass()
-                .getGenericSuperclass())
-                .getActualTypeArguments()[1];
-        this.list = getList(this.typeDomain.getName());
+        this.list = this.getMyList();
     }
 
     @Override
-    public List<DOMAIN> consultar() {
+    public List<T> consultar() {
         return list;
     }
 
     @Override
-    public Optional<DOMAIN> consultar(ID id) {
-        final var type = ((ParameterizedType) getClass()
-                .getGenericSuperclass())
-                .getActualTypeArguments()[0];
-        return list.stream().filter(x -> {
-            return id.equals(this.getId(x));
-        }).findFirst();
-    }
-
-    private ID getId(DOMAIN x) {
-        try {
-            var localId = x.getClass().getMethod("getId");
-            return (ID) localId.invoke(x);
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
+    public Optional<T> consultar(ID id) {
+        return list.stream().filter(x -> id.equals(this.getId(x))).findFirst();
     }
 
     @Override
-    public List<DOMAIN> consultar(List<ID> ids) {
+    public List<T> consultar(List<ID> ids) {
         return ids.stream().map(id -> consultar(id).orElseThrow(NaoExisteException::new)).toList();
     }
 
     @Override
-    public void cadastrar(DOMAIN domain) {
+    public void cadastrar(T domain) {
         list.add(domain);
     }
 
     @Override
-    public void remover(DOMAIN domain) {
+    public void remover(T domain) {
         list.remove(domain);
+    }
+
+    private ID getId(T x) {
+        try {
+            return (ID) x.getClass().getMethod("getId").invoke(x);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ex) {
+            throw new NotImplementedException("Método: getId() não implementado", ex);
+        }
+    }
+
+    private Class<T> getTypeDomain() {
+        return (Class<T>) ((ParameterizedType) getClass()
+                .getGenericSuperclass())
+                .getActualTypeArguments()[1];
+    }
+
+    private List<T> getMyList() {
+        final String key = this.getTypeDomain().getName();
+        List<T> myList = (List<T>) MAP_DB.get(key);
+        if (Objects.isNull(myList)) {
+            myList = new ArrayList<>();
+            MAP_DB.put(key, myList);
+        }
+        return myList;
     }
 }
